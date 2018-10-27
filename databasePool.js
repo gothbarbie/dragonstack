f96d2b874a776a8da66f34d05833bf3ca57a0c57
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-27 17:54:23 +0200

    chore: test pool and comment out for future reference

--- databasePool.js
+++ databasePool.js
@@ -6,3 +6,10 @@ const databaseConfiguration = require('./secrets/databaseConfiguration')
 const pool = new Pool(databaseConfiguration)
 
 module.exports = pool
+
+// Un-comment and run file to test database connection
+// pool.query('SELECT * FROM generation', (error, response) => {
+//   if (error) return console.error('error', error)
+
+//   console.log('response', response.rows)
+// })
