7eb26324d5bbaf615b275f04d42947d1efc6cf1f
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-27 17:53:53 +0200

    chore: add generationId

--- app/generation/index.js
+++ app/generation/index.js
@@ -1,5 +1,4 @@
 const Dragon = require('../dragon')
-
 const { REFRESH_RATE, SECONDS } = require('../config')
 
 const refreshRate = REFRESH_RATE * SECONDS
@@ -7,6 +6,7 @@ const refreshRate = REFRESH_RATE * SECONDS
 class Generation {
   constructor() {
     this.expiration = this.calculateExpiration()
+    this.generationId = undefined
   }
 
   calculateExpiration() {
@@ -25,7 +25,7 @@ class Generation {
       throw new Error(`This generation expired on ${this.expiration}`)
     }
 
-    return new Dragon()
+    return new Dragon({ generationId: this.generationId })
   }
 }
 
