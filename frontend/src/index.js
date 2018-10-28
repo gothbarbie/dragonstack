2b88bab29ac04b0ac5ca823dede927411a55ba62
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-28 12:03:22 +0100

    chore: add App component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import { render } from 'react-dom'
+import styled from 'styled-components'
 
 import Generation from './components/Generation'
+import Dragon from './components/Dragon'
+
+const App = styled.div`
+  width: 100%;
+  min-width: 500px;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: 'Open Sans', sans-serif;
+  color: #444;
+`
 
 render(
-  <div>
-    <h2>Dragon Stack React Application</h2>
-    <Generation />
-  </div>,
+  <App>
+    <div>
+      <h1>Dragon Stack</h1>
+      <h2>There be dragons</h2>
+      <Generation />
+      <Dragon />
+    </div>
+  </App>,
   document.getElementById('root')
 )
