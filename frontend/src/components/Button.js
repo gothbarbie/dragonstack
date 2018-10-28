4b1c4edce85aeca213a3b45405d1df6cc857e52f
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-28 12:03:39 +0100

    chore: create Button component

---
+++ frontend/src/components/Button.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+  padding: 1rem;
+  background: #efefef;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  outline: none;
+  cursor: pointer;
+  font-size: 1rem;
+`
+
+export default Button
